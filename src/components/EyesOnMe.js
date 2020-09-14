87631e4c2ca6d13968b010aee52dc301fa5ab8fc
smakaitakori <makait87@icloud.com>
2020-09-14 19:35:51 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+    render(){
+        let focus= () => console.log('Good!');
+        let blur= () => console.log('Hey! Eyes on me!');
+
+        return(
+            <button 
+            onFocus={focus}
+            onBlur={blur} />
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+    render(){
+        let print = () => console.log('Entering password...');
+        return (
+            <input 
+            onKeyUp={print} 
+            type="password" />
+        )
+
+    }
+
+}
